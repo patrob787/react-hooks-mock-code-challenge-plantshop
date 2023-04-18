436aa42c478f18edfbd09152a2cb9deb11db1cc9
Patrick Roberts <patrob22@gmail.com>
2023-04-18 10:37:46 -0400

    third deliverable

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ plant }) {
+  const [inStock, setInStock] = useState(true)
+  
+  function handleClick() {
+    setInStock(false);
+  }
+  
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: ${plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {inStock ? (
+        <button onClick={handleClick} className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
